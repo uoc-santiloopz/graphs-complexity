19d746025e54b3496c8ee78e82a8d7b647384203
Santiago Lopez <santiago.lopez@i2cat.net>
2019-10-18 18:47:35 +0200

    mastered string interpolation ^^

--- mod1-previous-concepts/1_r-ordered-sample.py
+++ mod1-previous-concepts/1_r-ordered-sample.py
@@ -1,2 +1,15 @@
-name = input('What is your name?\n')
-print ('Hi, %s.' % name)
+# Let me know the formula!
+print ('You know, the formula for a r-ordered-sample with repetition is the following:\n')
+print ('VR(n,r) = n^r\n')
+print ('Where n is the number of ELEMENTS in the SET, and R is the samples taken in the function\n')
+print ('///////////////////////////////\n')
+print ('///////////////////////////////\n')
+print ('///////////////////////////////\n')
+print ('///////////////////////////////\n')
+print ('NOW.... LET US CALCULATE!!!\n')
+
+
+# CALCULATE!
+n = input('How many ELEMENTS does the SET have?\n')
+r = input('How many samples are taken?\n')
+print ('So... we want to calculate VR(%(n)s, %(r)s) = %(n)s^%(r)s' % { "n": n, "r": r})
