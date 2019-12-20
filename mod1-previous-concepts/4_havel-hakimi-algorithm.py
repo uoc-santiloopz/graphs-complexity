30c3c52ea3daad21a983073fc09c41a16fb0a738
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-20 18:17:59 +0100

    sorting list

--- mod1-previous-concepts/4_havel-hakimi-algorithm.py
+++ mod1-previous-concepts/4_havel-hakimi-algorithm.py
@@ -3,8 +3,13 @@ from utils import to_int
 print("You are about to witness the power of the Havel-Hakimi algorithm...\n")
 print("Enter your sequence of numbers, separated by spaces\n")
 
+# variable to store the final result
+graphical_seq = False
+# ask for the space separated list
 seq = input("What is the sequence?\n").split()
+# map the list into integers and sort it
 mapped = list(map(to_int, seq))
+mapped.sort()
 
 
-print("Final sequence", list(mapped))
+print("Final sequence", mapped)
