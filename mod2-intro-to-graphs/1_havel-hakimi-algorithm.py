76b2409fd09ead024f00d397e9c41761f552d574
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-23 19:25:35 +0100

    finished algorithm, proceed to testing

--- mod2-intro-to-graphs/1_havel-hakimi-algorithm.py
+++ mod2-intro-to-graphs/1_havel-hakimi-algorithm.py
@@ -21,25 +21,37 @@ print(sortedIntSeq)
 # else:
 #     print("The sequence is not graphical\n")
 
+
 # Havel-Hakimi algorithm, determines weather a sequence of numbers can be
 # the degrees of the edges of a simple graph
 def sequence_is_graphic(seq):
 
+    # check if there is some edge with a degree exceeding the total number of edges minus 1
+    if max(seq) > len(seq) - 1:
+        return False
+
     # Perform operations until one of the stopping conditions is met
 
     # sort the sequence
-    sortedIntSeq = sorted(seq, reverse=True)
+    sorted_int_seq = sorted(seq, reverse=True)
 
     # check if all elements are equal to 0
-    if sortedIntSeq[0] == 0 and sortedIntSeq[len(sortedIntSeq)-1] == 0:
+    if sorted_int_seq[0] == 0 and sorted_int_seq[len(sorted_int_seq)-1] == 0:
         return True
 
     # store the first element in a variable and delete it from the list
-    firstEl = sortedIntSeq[0]
-    sortedIntSeq = sortedIntSeq[:1]
+    first_el = sorted_int_seq[0]
+    sorted_int_seq = sorted_int_seq[:1]
 
     # check if there are elements left of the list
-    if len(firstEl) > len(sortedIntSeq):
+    if len(first_el) > len(sorted_int_seq):
         return False
 
-    # substract the first element from the
+    # subtract the first element from the
+    for i in range(first_el):
+        sorted_int_seq[i] -= 1
+
+        if sorted_int_seq[i] < 0:
+            return False
+
+    sequence_is_graphic(sorted_int_seq)
