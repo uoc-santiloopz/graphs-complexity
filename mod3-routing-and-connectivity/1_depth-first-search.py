8e25e4f912976ea28fb811a825ec85d3a28d970a
Santiago Lopez <santiago.lopez@i2cat.net>
2020-01-03 18:52:25 +0100

    added all possible paths algorithm

--- mod3-routing-and-connectivity/1_depth-first-search.py
+++ mod3-routing-and-connectivity/1_depth-first-search.py
@@ -12,13 +12,23 @@ def dfs(grph, start):
     visited, stack = set(), [start]
     while stack:
         vertex = stack.pop()
-        print(stack)
-        print(vertex)
         if vertex not in visited:
             visited.add(vertex)
             stack.extend(grph[vertex] - visited)
     return visited
 
 
+def dfs_paths(grph, start, goal, path=None):
+    if path is None:
+        path = [start]
+    if start == goal:
+        yield path
+    for nxt in grph[start] - set(path):
+        yield from dfs_paths(grph, nxt, goal, path + [nxt])
+
+
 # {'E', 'D', 'F', 'A', 'C', 'B'}
 print(dfs(graph, 'A'))
+
+# [['C', 'F'], ['C', 'A', 'B', 'E', 'F']]
+print(list(dfs_paths(graph, 'C', 'F')))
